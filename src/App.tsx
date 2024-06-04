3a1e363ebb5aa889a9873f0c0ed53e4e72c123cb
corelabui <corelabui@gmail.com>
2024-06-04 09:59:57 +0200

    Chore: use latest formfusion styled version

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,6 @@ const MyForm = () => {
         name="credit-card-number-hyphen"
         type="credit-card-number-hyphen"
         mask="####-####-####-####"
-        classes={{
-          root: 'input',
-          field: 'input__field',
-          label: 'input__field__label',
-          error: 'input__field__error-message',
-        }}
       />
       <button type="submit">Submit</button>
     </Form>
